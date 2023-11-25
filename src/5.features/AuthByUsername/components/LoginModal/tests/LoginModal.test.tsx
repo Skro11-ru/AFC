3c8ad56665301d0ea7dee6a23e6b9c6f667f6c5d
Skro11 <bunetskydn@gmail.com>
2023-11-26 01:16:59 +0700

    fix(main): fix after linting

--- src/5.features/AuthByUsername/components/LoginModal/tests/LoginModal.test.tsx
+++ src/5.features/AuthByUsername/components/LoginModal/tests/LoginModal.test.tsx
@@ -1,8 +1,8 @@
-// import { render } from '@testing-library/react';
-// import { LoginModal } from '../LoginModal';
-//
-// describe('LoginModal', () => {
-// 	test('Test render', () => {
-// 		render(<LoginModal isOpen={true}  onClose={}/>);
-// 	});
-// });
+import { render } from '@testing-library/react';
+import { LoginModal } from '../LoginModal';
+
+describe('LoginModal', () => {
+	test('Test render', () => {
+		render(<LoginModal isOpen onClose={() => true} />);
+	});
+});
